e1e4f4ebb1e9b5055379aa4b20e432e72e1c5434
nabajit-cpu <nabajitboro796@gmail.com>
2022-09-09 21:10:44 +0530

    final

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts';
+const url = window.location.hostname==="localhost"?"http://localhost:5000/posts":"https://memories-server-side-main.herokuapp.com/posts";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
